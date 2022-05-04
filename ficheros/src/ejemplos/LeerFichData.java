2ef856db0d5aaa624198891ade4678081adb764e
shovasofi <90852177+shovasofi@users.noreply.github.com>
2022-05-04 17:28:40 +0200

    Add files via upload

---
+++ ficheros/src/ejemplos/LeerFichData.java
@@ -0,0 +1,37 @@
+package ejemplos;
+
+import java.io.*;
+
+public class LeerFichData {
+    public static void main(String[] args) throws IOException {
+	File fichero = new File("files/FichData.dat");
+	
+	int contador=0;
+	try (	DataInputStream dataIS = new DataInputStream(
+		new FileInputStream(fichero));) {
+		
+		// campos que se van a ir leyendo del fichero
+		String n;
+		int e;
+		
+	    // recorremos el fichero usando un bucle "infinito"
+	    // al llegar al final, se producirá la excepción EOFException
+		//while (dataIS.available()>0) con este no habría que poner las excepciones
+	    while (true) {
+			n = dataIS.readUTF(); // recupera el nombre
+			e = dataIS.readInt(); // recupera la edad
+			System.out.println("Nombre: " + n + ", edad: " + e);
+			contador++;
+	    }
+
+	} catch (EOFException eo) {
+	    System.out.format("%nSe han leido correctamente %d   registros",
+			contador);
+	
+
+	
+
+    }
+}
+    
+}
---
+++ ficheros/src/ejercicio9CopiaDeFicheros/CopiaArchivos.java
@@ -0,0 +1,61 @@
+package ejercicio9CopiaDeFicheros;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CopiaArchivos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		File fichero_leer = null;
+		File fichero_escribir = null;
+		
+		//leer la ruta
+		String ruta,nombre_org,nombre_copia;
+		
+	
+		System.out.println("Inserte la ruta  del fichero");
+		ruta=sc.nextLine();
+		System.out.println("Inserte el nombre del fichero");
+		nombre_org=sc.nextLine();
+		System.out.println("Inserte el nombre del fichero copia");
+		nombre_copia=sc.nextLine();
+		
+		try {
+			fichero_leer= new File(ruta + "/" + nombre_org);
+			fichero_escribir= new File(ruta+ "/" + nombre_copia);
+		}catch(Exception e) {
+			System.out.println("La ruta, nombre origen o nombre copia son errorneas, no se ha podido abrir el fichero");
+			System.exit(1);
+		}
+	
+		
+		try(
+				FileInputStream fis = new FileInputStream (fichero_leer );
+				FileOutputStream fos = new FileOutputStream(  fichero_escribir);){
+		
+			int unByte;
+
+			unByte= fis.read();
+			while ( unByte != -1){
+
+			//la marca de fin de fichero es -1
+			fos.write(unByte);
+			unByte= fis.read();
+
+			}
+			System.out.println("Copia realizada correctamente");
+			fis. close();
+			fos.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
